288e1e0f1a05a53d2b8b45264e3dfe7dc5994cc2
Gokul Kanish <66904387+imgokulkanish@users.noreply.github.com>
2021-07-30 17:21:19 +0530

    ProGrad ID

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-
+window.onload = () => {
+    displayItems()
+  }
+  let parentlist = document.getElementById('list')
+  let income = 0
+  let expense = 0
+  function displayItems() {
+    for (let i = 0; i < localStorage.length; i++) {
+      let list = document.createElement('li')
+      let amount = localStorage.getItem(localStorage.key(i))
+      let input = localStorage.key(i)
+      if (amount > 0) {
+        income = income + parseInt(amount)
+        list.setAttribute('class', 'plus')
+        list.textContent = input + '       ' + amount
+        // list.appendChild(document.createTextNode(input + '    ' + amount))
+        parentlist.appendChild(list)
+      } else {
+        expense -= amount
+        list.setAttribute('class', 'minus')
+        list.textContent = input + '       ' + amount
+        parentlist.appendChild(list)
+      }
+    }
+    document.getElementById('money-plus').textContent = '$' + income
+    document.getElementById('money-minus').textContent = '$' + expense
+    document.getElementById('balance').textContent = '$' + (income - expense)
+  }
+  let button = document.getElementById('btn')
+  button.addEventListener('click', addItems)
+  
+  function addItems() {
+    var input = document.getElementById('text').value
+    let amount = document.getElementById('amount').value
+    window.localStorage.setItem(input, amount)
+  }
